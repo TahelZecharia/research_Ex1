bcca00d63dd2cac57ed482e30d74ac599245794b
TahelZecharia <93916792+TahelZecharia@users.noreply.github.com>
2022-11-02 04:39:18 +0200

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,3 +1,6 @@
+"""
+Tahel Zecharia
+"""
 
 """
 ************* Q1 *************
@@ -145,8 +148,69 @@ def rec_sorted(x):
 
 """
 ************* Q4 *************
-https://www.codingame.com/training/easy/onboarding
+https://www.codingame.com/ide/puzzle/power-of-thor-episode-1
 
+my solution:
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+# ---
+# Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
+# light_x: the X position of the light of power
+# light_y: the Y position of the light of power
+# initial_tx: Thor's starting X position
+# initial_ty: Thor's starting Y position
+
+light_x, light_y, initial_tx, initial_ty = [int(i) for i in input().split()]
+curr_x = initial_tx
+curr_y = initial_ty
+
+# game loop
+while True:
+    remaining_turns = int(input())  # The remaining amount of turns Thor can move. Do not remove this line.
+
+    if(curr_x < light_x):
+        if(curr_y < light_y):
+            curr_x+=1
+            curr_y+=1
+            print("SE")
+        elif(curr_y > light_y):
+            curr_x+=1
+            curr_y-=1
+            print("NE")
+        else:
+           curr_x+=1 
+           print("E")
+
+    elif(curr_x > light_x):
+        if(curr_y > light_y):
+            curr_x-=1
+            curr_y-=1
+            print("NW")
+        elif(curr_y < light_y):
+            curr_x-=1
+            curr_y+=1
+            print("SW")
+        else:
+           curr_x-=1 
+           print("W")
+    
+    elif(curr_y < light_y):
+        curr_y+=1
+        print("S")
+
+    elif(curr_y > light_y):
+        curr_y-=1
+        print("N")
+        
+        
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    # A single line providing the move to be made: N NE E SE S SW W or NW
+    
 """
 if __name__ == '__main__':
 
